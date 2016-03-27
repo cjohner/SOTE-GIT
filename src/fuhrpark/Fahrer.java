14b0835ce290da1c9d830d33eaf4eac9fb970ac0
Dr. Christian Johner <mail@johner.org>
2016-03-27 15:36:51 +0200

    Fahrer id von int nach String geändert

--- src/fuhrpark/Fahrer.java
+++ src/fuhrpark/Fahrer.java
@@ -2,14 +2,14 @@ package fuhrpark;
 
 public class Fahrer {
 	private String name;
-	private int fahrerId;
+	private String fahrerId;
 	
 	/**
 	 * 
 	 * @param name: Vor- und Nachname 
 	 * @param fahrerId: kann nicht mehr geaendert werden
 	 */
-	public Fahrer(String name, int fahrerId) {
+	public Fahrer(String name, String fahrerId) {
 		this.name = name;
 		this.fahrerId = fahrerId;
 	}
@@ -19,7 +19,7 @@ public class Fahrer {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getFahrerId() {
+	public String getFahrerId() {
 		return fahrerId;
 	}
 }
