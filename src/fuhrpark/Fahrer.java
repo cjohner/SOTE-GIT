2806e5fe2f51f1e1644f180a52a7590a4198ec42
Dr. Christian Johner <mail@johner.org>
2016-03-27 15:32:57 +0200

    Kommentar zu Fahrer hinzugefügt

--- src/fuhrpark/Fahrer.java
+++ src/fuhrpark/Fahrer.java
@@ -4,6 +4,11 @@ public class Fahrer {
 	private String name;
 	private int fahrerId;
 	
+	/**
+	 * 
+	 * @param name: Vor- und Nachname 
+	 * @param fahrerId: kann nicht mehr geaendert werden
+	 */
 	public Fahrer(String name, int fahrerId) {
 		this.name = name;
 		this.fahrerId = fahrerId;
